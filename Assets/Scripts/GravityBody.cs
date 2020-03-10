091daacbbe8e1c6db961acadf03982528e44a4b2
nitin <nitinprmr75@gmail.com>
2020-03-10 22:56:38 +0530

    Add Planet Gravity Demo

---
+++ Assets/Scripts/GravityBody.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// GameObject of body this will be attract by GravityAttractor
+/// </summary>
+[RequireComponent(typeof(Rigidbody))]
+public class GravityBody : MonoBehaviour
+{
+    // Gravity Attractor reference
+    PlanetGravityAttractor attractor;
+
+    // reference Rigidbody attached to game object
+    Rigidbody body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        attractor = GameObject.FindGameObjectWithTag("Planet").GetComponent<PlanetGravityAttractor>();
+        body = GetComponent<Rigidbody>();
+
+        body.useGravity = false;
+        body.constraints = RigidbodyConstraints.FreezeRotation;
+    }
+
+    // LateUpdate is called once after update
+    void LateUpdate()
+    {
+        if (attractor != null && body != null)
+        {
+            attractor.Attract(body);
+        }
+    }
+}
---
+++ Assets/Scripts/PlanetGravityAttractor.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlanetGravityAttractor : MonoBehaviour
+{
+    public Vector3 gravityCenter = Vector3.zero; // attractor center of gravity
+    public float gravitationConstant = -9.8f; // our attractor gravity
+
+    private float attractorMass; // mass of our planet
+
+    void Start()
+    {
+        attractorMass = this.GetComponent<Rigidbody>().mass;
+    }
+
+    /// <summary>
+    /// Attract other body to this GameObject
+    /// </summary>
+    /// <param name="Body"></param>
+    /// <returns></returns>
+    public void Attract(Rigidbody attractedBody)
+    {
+        Vector3 pullVector = FindSurfaceNormal(attractedBody); // Find the surface normal of Attractor
+        OrientBody(attractedBody, pullVector); // Orient the player to surface normal
+
+        // Calculate the pull force of attractor
+        float pullForce = 0.0f;
+        float attractedBodyDistance = Vector3.Distance(this.transform.position + gravityCenter, attractedBody.transform.position);
+        
+        // Inverse square law - GravityConstant * ((mass1 * mass2) / distance^2)
+        pullForce = gravitationConstant * ((attractorMass * attractedBody.mass) / Mathf.Pow(attractedBodyDistance, 2));
+        
+        // find direction to attractedbody
+        pullVector = attractedBody.transform.position - gravityCenter;
+        // apply gravitation force
+        attractedBody.AddForce(pullVector.normalized * pullForce * Time.deltaTime);
+    }
+
+    /// <summary>
+    /// Find the surface normal of planet
+    /// used for rotation for attractedBody according to 
+    /// surface of planet
+    /// </summary>
+    /// <param name="attractedBody"></param>
+    /// <returns></returns>
+    Vector3 FindSurfaceNormal(Rigidbody attractedBody)
+    {
+        float distance = Vector3.Distance(this.transform.position, attractedBody.position);
+        Vector3 surfaceNorm = Vector3.zero;
+
+        RaycastHit hit;
+        if (Physics.Raycast(attractedBody.transform.position, -attractedBody.transform.up, out hit, distance))
+        {
+            surfaceNorm = hit.normal;
+        }
+        return surfaceNorm;
+    }
+
+    /// <summary>
+    /// Orient attracted body to surface normal of attractor
+    /// </summary>
+    /// <param name="attractedBody"></param>
+    /// <param name="surfaceNormal"></param>
+    /// <returns></returns>
+    void OrientBody(Rigidbody attractedBody, Vector3 surfaceNormal)
+    {
+        attractedBody.transform.rotation = Quaternion.FromToRotation(attractedBody.transform.up, surfaceNormal) * attractedBody.rotation;
+    }
+}
